40855186217834cff8accaf6c87fc3f202647280
AustinSweet <austinsweet@rightnowmedia.org>
2021-05-28 20:22:37 -0500

    Added second var for CAP-22 substask

--- Driver.java
+++ Driver.java
@@ -2,6 +2,7 @@ public class Driver {
 
     public static void main(String[] xxx) {
         int x = 1;
-        System.out.println(x);
+        int y = 2;
+        System.out.println(x + " " + y);
     }
 }
